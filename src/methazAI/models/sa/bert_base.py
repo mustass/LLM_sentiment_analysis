4e635c809e9eb7ef1fd18baa69e7ed387e856658
Stas Magdych <mustas@pm.me>
2022-10-07 10:21:54 +0200

    installs on HPC

--- src/methazAI/models/sa/bert_base.py
+++ src/methazAI/models/sa/bert_base.py
@@ -1,12 +1,13 @@
+from turtle import forward
 from omegaconf import DictConfig
 from regex import X
 from torch import nn
 
 from methazAI.utils.technical_utils import load_obj
-from transformers import BertForSequenceClassification
+from transformers import BertModel, BertForSequenceClassification
+import torch.nn.functional as F
 
-
-class BERT(nn.Module):
+class BERT4SC(nn.Module):
     def __init__(self, cfg: DictConfig) -> None:
         """
         Model class.
@@ -32,3 +33,33 @@ class BERT(nn.Module):
     def forward(self, x, mask):
         output = self.model(x, mask)
         return output['logits']
+
+class BERT(nn.Module):
+    def __init__(self, cfg: DictConfig) -> None:
+        """
+        Model class.
+
+        Args:
+            cfg: main config
+        """
+        super(BERT, self).__init__()
+
+        self.bert = BertModel.from_pretrained("bert-base-uncased")
+        self.dropout = nn.Dropout(cfg.model.params.dropout)
+        self.linear = nn.Linear(768, cfg.model.params.n_clas)
+
+
+        if cfg.model.params.finetune:
+            for param in self.bert.parameters():
+                param.requires_grad = False
+
+            for param in self.linear.parameters():
+                param.requires_grad = True
+
+    def forward(self, input_id, mask):
+        _, pooled_output = self.bert(input_ids= input_id, attention_mask=mask,return_dict=False)
+        dropout_output = self.dropout(pooled_output)
+        linear_output = self.linear(dropout_output)
+        final_layer = F.relu(linear_output)
+        output = F.softmax(final_layer,dim=1)
+        return output
